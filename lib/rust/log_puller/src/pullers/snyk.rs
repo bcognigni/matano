247dab07925e8ff990221563f1b1b5d0e9e86c58
Bruno Cognigni <bruno.cognigni@pomelo.la>
2024-03-01 12:01:00 -0300

    Update snyk.rs
    Testing snyk response
    
    Signed-off-by: Bruno Cognigni <bruno.cognigni@pomelo.la>

--- lib/rust/log_puller/src/pullers/snyk.rs
+++ lib/rust/log_puller/src/pullers/snyk.rs
@@ -238,6 +238,7 @@ impl PullLogs for SnykPuller {
                     .await?;
 
                 let response_json: Vec<serde_json::Value> = response.json().await?;
+                info!("This is the response from reporting: {}", &response_json);
                 let length = response_json.len();
 
                 for mut value in response_json {
